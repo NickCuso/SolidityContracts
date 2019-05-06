213f3c25e4411b9515107e88751d1a343b23d129
hardlydifficult <hardlydiff@gmail.com>
2019-05-06 16:41:08 -0700

    lint

--- tron/contracts/openzeppelin/access/roles/WhitelistAdminRole.sol
+++ tron/contracts/openzeppelin/access/roles/WhitelistAdminRole.sol
@@ -7,41 +7,41 @@ import "../Roles.sol";
  * @dev WhitelistAdmins are responsible for assigning and removing Whitelisted accounts.
  */
 contract WhitelistAdminRole {
-    using Roles for Roles.Role;
+  using Roles for Roles.Role;
 
-    event WhitelistAdminAdded(address indexed account);
-    event WhitelistAdminRemoved(address indexed account);
+  event WhitelistAdminAdded(address indexed account);
+  event WhitelistAdminRemoved(address indexed account);
 
-    Roles.Role private _whitelistAdmins;
+  Roles.Role private _whitelistAdmins;
 
-    constructor () internal {
-        _addWhitelistAdmin(msg.sender);
-    }
+  constructor () internal {
+    _addWhitelistAdmin(msg.sender);
+  }
 
-    modifier onlyWhitelistAdmin() {
-        require(isWhitelistAdmin(msg.sender));
-        _;
-    }
+  modifier onlyWhitelistAdmin() {
+    require(isWhitelistAdmin(msg.sender));
+    _;
+  }
 
-    function isWhitelistAdmin(address account) public view returns (bool) {
-        return _whitelistAdmins.has(account);
-    }
+  function isWhitelistAdmin(address account) public view returns (bool) {
+    return _whitelistAdmins.has(account);
+  }
 
-    function addWhitelistAdmin(address account) public onlyWhitelistAdmin {
-        _addWhitelistAdmin(account);
-    }
+  function addWhitelistAdmin(address account) public onlyWhitelistAdmin {
+    _addWhitelistAdmin(account);
+  }
 
-    function renounceWhitelistAdmin() public {
-        _removeWhitelistAdmin(msg.sender);
-    }
+  function renounceWhitelistAdmin() public {
+    _removeWhitelistAdmin(msg.sender);
+  }
 
-    function _addWhitelistAdmin(address account) internal {
-        _whitelistAdmins.add(account);
-        emit WhitelistAdminAdded(account);
-    }
+  function _addWhitelistAdmin(address account) internal {
+    _whitelistAdmins.add(account);
+    emit WhitelistAdminAdded(account);
+  }
 
-    function _removeWhitelistAdmin(address account) internal {
-        _whitelistAdmins.remove(account);
-        emit WhitelistAdminRemoved(account);
-    }
+  function _removeWhitelistAdmin(address account) internal {
+    _whitelistAdmins.remove(account);
+    emit WhitelistAdminRemoved(account);
+  }
 }
--- tron/contracts/openzeppelin/access/roles/WhitelistedRole.sol
+++ tron/contracts/openzeppelin/access/roles/WhitelistedRole.sol
@@ -10,41 +10,41 @@ import "./WhitelistAdminRole.sol";
  * it), and not Whitelisteds themselves.
  */
 contract WhitelistedRole is WhitelistAdminRole {
-    using Roles for Roles.Role;
+  using Roles for Roles.Role;
 
-    event WhitelistedAdded(address indexed account);
-    event WhitelistedRemoved(address indexed account);
+  event WhitelistedAdded(address indexed account);
+  event WhitelistedRemoved(address indexed account);
 
-    Roles.Role private _whitelisteds;
+  Roles.Role private _whitelisteds;
 
-    modifier onlyWhitelisted() {
-        require(isWhitelisted(msg.sender));
-        _;
-    }
+  modifier onlyWhitelisted() {
+    require(isWhitelisted(msg.sender));
+    _;
+  }
 
-    function isWhitelisted(address account) public view returns (bool) {
-        return _whitelisteds.has(account);
-    }
+  function isWhitelisted(address account) public view returns (bool) {
+    return _whitelisteds.has(account);
+  }
 
-    function addWhitelisted(address account) public onlyWhitelistAdmin {
-        _addWhitelisted(account);
-    }
+  function addWhitelisted(address account) public onlyWhitelistAdmin {
+    _addWhitelisted(account);
+  }
 
-    function removeWhitelisted(address account) public onlyWhitelistAdmin {
-        _removeWhitelisted(account);
-    }
+  function removeWhitelisted(address account) public onlyWhitelistAdmin {
+    _removeWhitelisted(account);
+  }
 
-    function renounceWhitelisted() public {
-        _removeWhitelisted(msg.sender);
-    }
+  function renounceWhitelisted() public {
+    _removeWhitelisted(msg.sender);
+  }
 
-    function _addWhitelisted(address account) internal {
-        _whitelisteds.add(account);
-        emit WhitelistedAdded(account);
-    }
+  function _addWhitelisted(address account) internal {
+    _whitelisteds.add(account);
+    emit WhitelistedAdded(account);
+  }
 
-    function _removeWhitelisted(address account) internal {
-        _whitelisteds.remove(account);
-        emit WhitelistedRemoved(account);
-    }
+  function _removeWhitelisted(address account) internal {
+    _whitelisteds.remove(account);
+    emit WhitelistedRemoved(account);
+  }
 }
